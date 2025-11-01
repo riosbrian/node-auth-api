43bf6dc078d6985c28a06c2fae0b245a9dc551a9
Brian Rios <riosbrian.ar@gmail.com>
2025-11-01 16:51:23 +0100

    chore(middleware): add error and not-found handlers

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,6 +1,6 @@
 import { get } from 'env-var';
 
 export const envs = {
-  NODE_ENV: get('NODE_ENV').default('development'),
+  NODE_ENV: get('NODE_ENV').default('development').asString(),
   PORT: get('PORT').required().asPortNumber(),
 };
